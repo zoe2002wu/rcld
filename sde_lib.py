890e47db28b4f4348b7e642a8b6fa7e55b5fe146
Zoe Wu <zoe2002wu@gmail.com>
2025-08-30 19:19:12 -0400

    G not defined

--- sde_lib.py
+++ sde_lib.py
@@ -58,9 +58,10 @@ class CLD(nn.Module):
 
         x, v = torch.chunk(u, 2, dim=1)
 
-        var_xx, var_xv, var_vv = self.var_constant(t[0].item())
+        var_xx, var_xv, var_vv = self.var_constant(t[0])
         det = var_xx * var_vv - var_xv**2
-        G_inv = det / (var_vv**2 + var_xv**2) 
+        G = (var_vv**2 + var_xv**2) / det
+        G_inv = 1 / G
 
         if t[0].item()<0.1:
             G_inv = self.m_inv
